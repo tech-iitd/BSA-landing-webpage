99dce70e5c3f5e72c3ed153ea2cc8bc7fde3099e
Lucky Bhuskute <142942957+codehub19@users.noreply.github.com>
2025-01-06 12:24:01 +0530

    hero section added

---
+++ src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './HeroSection.css';
+const HeroSection = () => {
+  return (
+      <div className='hero-section'>
+              <iframe className='bsa-3d-logo' src='https://my.spline.design/untitledcopy-6aefbfe4acc14ee2f8452acf95d83e76/' frameborder='0'></iframe>
+              <div className='hero-section-text'>
+              {/* <h1>IIT DELHI</h1> */}
+              </div>
+
+      </div>
+             
+  );
+}
+
+export default HeroSection;
