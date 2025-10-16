375b9aa447fd539c8be4635e570aea62d4c1db94
Satya Prakash <satyaprakash6945@gmail.com>
2025-10-16 13:44:57 +0530

    sports box update

--- src/components/SportsElement/SportsElement.jsx
+++ src/components/SportsElement/SportsElement.jsx
@@ -14,71 +14,110 @@ const SportsElement = () => {
         <h1>SPORTS</h1>
         <ul>
           <li>
-            <Link to="/sports/cricket">
-              <img src={sports.cricket.img} alt={sports.cricket.name} />
+            <Link to="/sports/cricket" className="sport-tile">
+              <div className="sport-image-wrap">
+                <img src={sports.cricket.img} alt={sports.cricket.name} />
+                <span className="sport-label">{sports.cricket.name}</span>
+              </div>
             </Link>
           </li>
           <li>
-            <Link to="/sports/football">
-              <img src={sports.football.img} alt={sports.football.name} />
+            <Link to="/sports/football" className="sport-tile">
+              <div className="sport-image-wrap">
+                <img src={sports.football.img} alt={sports.football.name} />
+                <span className="sport-label">{sports.football.name}</span>
+              </div>
             </Link>
           </li>
           <li>
-            <Link to="/sports/basketball">
-              <img src={sports.basketball.img} alt={sports.basketball.name} />
+            <Link to="/sports/basketball" className="sport-tile">
+              <div className="sport-image-wrap">
+                <img src={sports.basketball.img} alt={sports.basketball.name} />
+                <span className="sport-label">{sports.basketball.name}</span>
+              </div>
             </Link>
           </li>
           <li>
-            <Link to="/sports/volleyball">
-              <img src={sports.volleyball.img} alt={sports.volleyball.name} />
+            <Link to="/sports/volleyball" className="sport-tile">
+              <div className="sport-image-wrap">
+                <img src={sports.volleyball.img} alt={sports.volleyball.name} />
+                <span className="sport-label">{sports.volleyball.name}</span>
+              </div>
             </Link>
           </li>
           <li>
-            <Link to="/sports/lawnTennis">
-              <img src={sports.lawnTennis.img} alt={sports.lawnTennis.name} />
+            <Link to="/sports/lawnTennis" className="sport-tile">
+              <div className="sport-image-wrap">
+                <img src={sports.lawnTennis.img} alt={sports.lawnTennis.name} />
+                <span className="sport-label">{sports.lawnTennis.name}</span>
+              </div>
             </Link>
           </li>
           <li>
-            <Link to="/sports/badminton">
-              <img src={sports.badminton.img} alt={sports.badminton.name} />
+            <Link to="/sports/badminton" className="sport-tile">
+              <div className="sport-image-wrap">
+                <img src={sports.badminton.img} alt={sports.badminton.name} />
+                <span className="sport-label">{sports.badminton.name}</span>
+              </div>
             </Link>
           </li>
           <li>
-            <Link to="/sports/tableTennis">
-              <img src={sports.tableTennis.img} alt={sports.tableTennis.name} />
+            <Link to="/sports/tableTennis" className="sport-tile">
+              <div className="sport-image-wrap">
+                <img src={sports.tableTennis.img} alt={sports.tableTennis.name} />
+                <span className="sport-label">{sports.tableTennis.name}</span>
+              </div>
             </Link>
           </li>
           <li>
-            <Link to="/sports/chess">
-              <img src={sports.chess.img} alt={sports.chess.name} />
+            <Link to="/sports/chess" className="sport-tile">
+              <div className="sport-image-wrap">
+                <img src={sports.chess.img} alt={sports.chess.name} />
+                <span className="sport-label">{sports.chess.name}</span>
+              </div>
             </Link>
           </li>
           <li>
-            <Link to="/sports/athletics">
-              <img src={sports.athletics.img} alt={sports.athletics.name} />
+            <Link to="/sports/athletics" className="sport-tile">
+              <div className="sport-image-wrap">
+                <img src={sports.athletics.img} alt={sports.athletics.name} />
+                <span className="sport-label">{sports.athletics.name}</span>
+              </div>
             </Link>
           </li>
           <li>
-            <Link to="/sports/hockey">
-              <img src={sports.hockey.img} alt={sports.hockey.name} />
+            <Link to="/sports/hockey" className="sport-tile">
+              <div className="sport-image-wrap">
+                <img src={sports.hockey.img} alt={sports.hockey.name} />
+                <span className="sport-label">{sports.hockey.name}</span>
+              </div>
             </Link>
           </li>
           <li>
-            <Link to="/sports/squash">
-              <img src={sports.squash.img} alt={sports.squash.name} />
+            <Link to="/sports/squash" className="sport-tile">
+              <div className="sport-image-wrap">
+                <img src={sports.squash.img} alt={sports.squash.name} />
+                <span className="sport-label">{sports.squash.name}</span>
+              </div>
             </Link>
           </li>
           <li>
-            <Link to="/sports/aquatics">
-              <img src={sports.aquatics.img} alt={sports.aquatics.name} />
+            <Link to="/sports/aquatics" className="sport-tile">
+              <div className="sport-image-wrap">
+                <img src={sports.aquatics.img} alt={sports.aquatics.name} />
+                <span className="sport-label">{sports.aquatics.name}</span>
+              </div>
             </Link>
           </li>
           <li>
-            <Link to="/sports/weightlifting">
-              <img
-                src={sports.weightlifting.img}
-                alt={sports.weightlifting.name}
-              />
+            <Link to="/sports/weightlifting" className="sport-tile">
+              <div className="sport-image-wrap">
+                <img
+                  src={sports.weightlifting.img}
+                  alt={sports.weightlifting.name}
+                />
+                <span className="sport-label">{sports.weightlifting.name}</span>
+              </div>
             </Link>
           </li>
         </ul>
